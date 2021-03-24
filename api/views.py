ffea4672737e62a530a76e03b44877cf53cb3af1
Ilya <netri-x@yandex.ru>
2021-03-24 18:22:54 +0300

    Reviews and Comments

--- api/views.py
+++ api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+from .models import Review, Title
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (CommentSerializer, ReviewSerializer)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly
+    )
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly
+    )
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comments.all()
