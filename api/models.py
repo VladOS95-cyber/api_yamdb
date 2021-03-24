af4287dd15535b0586fe2d260c0680992b489f3a
Evgeny.Alekseev <Alekseev.e.m@mail.ru>
2021-03-24 19:43:53 +0300

    1st commit

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(null=True, blank=True)
+    description = models.CharField(max_length=1000, blank=True)
+    rating = models.IntegerField(blank=True, null=True)
+    genre = models.ManyToManyField(Genre, verbose_name='Жанр')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, verbose_name='Категория')
